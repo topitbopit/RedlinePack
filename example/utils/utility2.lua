249c61f9c2f82d5440d4fb6a0318b102e41629f7
topit <82128920+topitbopit@users.noreply.github.com>
2022-09-03 12:05:14 -0700

    Example project file

---
+++ example/utils/utility2.lua
@@ -0,0 +1,10 @@
+local utility2 = {}
+
+function utility2:isUtility() 
+    return true
+end
+function utility2:getUtilId() 
+    return 2
+end
+
+return utility2
---
+++ example/utils/utility3.lua
@@ -0,0 +1,10 @@
+local utility3 = {}
+
+function utility3:isUtility() 
+    return true
+end
+function utility3:getUtilId() 
+    return 3
+end
+
+return utility3
